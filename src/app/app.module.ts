b483ed7def5a6f9900a58d319159d4802155d177
Luis Felipe Fezer <luisfelipefezer@gmail.com>
2022-06-26 13:15:56 -0300

    basic Angular subtraction app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SubtracaoModule } from './subtracao';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SubtracaoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
